9641b52a7838003b7fa62dbd59875cfbfe60a23e
Nikolai <n.darinsky@gmail.com>
2020-10-27 17:33:18 +0300

    implement dijkstra algorithm

--- dijkstra.js
+++ dijkstra.js
@@ -1,6 +1,11 @@
-// [node1, node2, length]
-// node1 => node2
-const input = [
+const numberOfCities = 8
+
+// Number which will be treated as infinitely large for this conditions.
+const maxLength = 1000 
+
+// Describes road between 2 cities and length of this road
+const roads = [
+    [0,1,9],
     [1,2,4],
     [1,4,10],
     [2,3,21],
@@ -11,47 +16,48 @@ const input = [
     [6,3,12]
 ]
 
-const buildChild = (item) => {
-    return { id: item[1], length: item[2] }
+const nodes = []
+for (let i=0; i<numberOfCities; i++) {
+    nodes.push({
+        length: i ? maxLength : 0,
+        children: []
+    })
 }
 
-const buildNode = (item) => {
-    return {
-        id: item[0],
-        children: [buildChild(item)]
+roads.forEach(road => {
+    const key = road[0]
+    const node = nodes[key]
+    node.children.push({key: road[1], length: road[2]})
+})
+
+
+let current = 0
+const queue = []
+queue.push(0)
+
+const compare = (a,b) => {
+    if (a.length < b.length) {
+        return -1
     }
+    if (a.length > b.length) {
+        return 1
+    }
+    return 0
 }
 
-const buildGraf = (input) => {
-    const nodes = new Map()
-    
-    input.forEach(element => {
-        const key = element[0]
-        if (nodes.has(key)) {
-            nodes.get(key).children.push(buildChild(element))
-        } else {
-            nodes.set(key, buildNode(element))
+while(queue.length) {
+    current = queue.shift()
+    const node = nodes[current]
+
+    node.children.sort(compare)
+    node.children.forEach(child => {
+        const {key, length} = child
+        if (nodes[key].length > length) {
+            nodes[key].length = length + nodes[current].length
+            nodes[key].parent = current
+            queue.push(key)
         }
-    });
-
-    return nodes
+    })
 }
-
-const findShortestPath = (nodes, endNode) => {
-    const queue = []
-    const currentNode = nodes.get(1)
-    currentNode.length = 0
-
-    while (currentNode.id !== endNode) {
-        currentNode.children.forEach(child => {
-            
-            queue.push(child)
-        })
-    }
-    
-}
-
-const nodes = buildGraf(input)
-
 
 console.log(nodes)
